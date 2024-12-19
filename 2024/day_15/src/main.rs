1c1e76c18fe421c52835616de627df0edae23f77
hiibolt <me@hiibolt.com>
2024-12-19 15:35:44 -0600

    feat: Completed part two of `day_15` for 2024

--- 2024/day_15/src/main.rs
+++ 2024/day_15/src/main.rs
@@ -1,161 +1,15 @@
-enum Node {
-    Wall,
-    Box,
-    Free
-}
-impl Node {
-    fn display ( &self ) {
-        match self {
-            Node::Wall => print!("#"),
-            Node::Box => print!("O"),
-            Node::Free => print!(" ")
+fn print ( map: &Vec<Vec<char>> ) {
+    for row in map {
+        for node in row {
+            print!("{node}");
         }
+        println!();
     }
 }
-
-struct Robot {
-    i: usize,
-    g: usize
-}
-impl Robot {
-    fn mv (
-        &mut self,
-        board: &mut Vec<Vec<Node>>,
-        mv: char
-    ) {
-        println!("Moving {mv}");
-        match mv {
-            '^' => {
-                if self.i == 0 {
-                    return;
-                }
-
-                match board[self.i - 1][self.g] {
-                    Node::Wall => { },
-                    Node::Free => {
-                        self.i -= 1;
-                    },
-                    Node::Box => {
-                        for i in 0..(self.i - 2) {
-                            let i = self.i - i - 2;
-
-                            if let Node::Free = board[i][self.g] {
-                                board[i][self.g] = Node::Box;
-                                board[self.i - 1][self.g] = Node::Free;
-                                self.i -= 1;
-                                break;
-                            }
-                            if let Node::Wall = board[i][self.g] {
-                                break;
-                            }
-                        }
-                    }
-                }
-            },
-            'v' => {
-                if self.i == board.len() - 1 {
-                    return;
-                }
-
-                match board[self.i + 1][self.g] {
-                    Node::Wall => { },
-                    Node::Free => {
-                        self.i += 1;
-                    },
-                    Node::Box => {
-                        for i in (self.i + 2)..(board.len() - 1) {
-                            if let Node::Free = board[i][self.g] {
-                                board[i][self.g] = Node::Box;
-                                board[self.i + 1][self.g] = Node::Free;
-                                self.i += 1;
-                                break;
-                            }
-                            if let Node::Wall = board[i][self.g] {
-                                break;
-                            }
-                        }
-                    }
-                }
-            },
-            '<' => {
-                if self.g == 0 {
-                    return;
-                }
-
-                match board[self.i][self.g - 1] {
-                    Node::Wall => { },
-                    Node::Free => {
-                        self.g -= 1;
-                    },
-                    Node::Box => {
-                        for g in 0..(self.g - 2) {
-                            let g = self.g - g - 2;
-
-                            if let Node::Free = board[self.i][g] {
-                                board[self.i][g] = Node::Box;
-                                board[self.i][self.g - 1] = Node::Free;
-                                self.g -= 1;
-                                break;
-                            }
-                            if let Node::Wall = board[self.i][g] {
-                                break;
-                            }
-                        }
-                    }
-                }
-            },
-            '>' => {
-                if self.g == board[0].len() - 1 {
-                    return;
-                }
-
-                match board[self.i][self.g + 1] {
-                    Node::Wall => { },
-                    Node::Free => {
-                        self.g += 1;
-                    },
-                    Node::Box => {
-                        for g in (self.g + 2)..(board[0].len() - 1) {
-                            if let Node::Free = board[self.i][g] {
-                                board[self.i][g] = Node::Box;
-                                board[self.i][self.g + 1] = Node::Free;
-                                self.g += 1;
-                                break;
-                            }
-                            if let Node::Wall = board[self.i][g] {
-                                break;
-                            }
-                        }
-                    }
-                }
-            },
-            _ => {}
-        }
-    }
-    fn render (
-        &self,
-        board: &Vec<Vec<Node>>
-    ) {
-        for i in 0..board.len() {
-            for g in 0..board[i].len() {
-                if i == self.i && g == self.g {
-                    print!("@");
-                    continue;
-                }
-
-                board[i][g].display();
-            }
-            println!();
-        }
-    }
-}
-
-
 fn main() {
     let data = std::fs::read_to_string("input.txt")
         .unwrap();
 
-    let mut robot: Option<Robot> = None;
     let moves = data.split("\n\n")
         .nth(1).unwrap()
         .lines()
@@ -164,54 +18,297 @@ fn main() {
     let mut map = data.split("\n\n")
         .next().unwrap()
         .lines()
-        .enumerate()
-        .map(|(i, ln)| {
+        .map(|ln| {
             ln.chars()
-                .enumerate()
-                .map(|(g, ch)| {
+                .flat_map(|ch| {
                     match ch {
-                        '#' => Node::Wall,
-                        'O' => Node::Box,
-                        '.' => Node::Free,
-                        '@' => {
-                            robot = Some(Robot {
-                                i,
-                                g
-                            });
-                            Node::Free
-                        },
-                        _  => Node::Free
+                        '.' => vec!('.', '.'),
+                        '#' => vec!('#', '#'),
+                        'O' => vec!('[', ']'),
+                        '@' => vec!('@', '.'),
+                        _ => vec!()
                     }
                 })
-                .collect::<Vec<Node>>()
+                .collect::<Vec<char>>()
         })
-        .collect::<Vec<Vec<Node>>>();
+        .collect::<Vec<Vec<char>>>();
 
-    let mut robot = robot.unwrap();
+    print(&map);
+    'move_loop: for mv in &moves {
+        match mv {
+            '<' => {
+                'outer: for i in 0..map.len() {
+                    for g in 0..map[i].len() {
+                        if map[i][g] != '@' { continue; }
+
+                        if g == 0 { 
+                            continue 'move_loop;
+                        }
+
+                        let left = map[i][g - 1];
+                        match left {
+                            '.' => {
+                                map[i][g] = '.';
+                                map[i][g - 1] = '@';
+                            },
+                            ']' => {
+                                for search_g in 0..(g - 3) {
+                                    let search_g = g - search_g - 3;
+
+                                    println!("Checking ({i}, {search_g}) ({})", map[i][search_g]);
+
+                                    if map[i][search_g] == '#' {
+                                        break;
+                                    }
+                                    if map[i][search_g] == '.' {
+                                        for move_g in search_g..(g - 1) {
+                                            map[i][move_g] = map[i][move_g + 1];
+                                        }
+                                        map[i][g - 1] = '@';
+                                        map[i][g] = '.';
+                                        break;
+                                    }
+                                }
+                            },
+                            _ => {}
+                        }
+
+                        break 'outer;
+                    }
+                }
+            },
+            '>' => {
+                'outer: for i in 0..map.len() {
+                    for g in 0..map[i].len() {
+                        if map[i][g] != '@' { continue; }
+
+                        if g == map[i].len() - 1 { 
+                            continue 'move_loop;
+                        }
+
+                        let right = map[i][g + 1];
+                        println!("Character on right: {right}");
+                        match right {
+                            '.' => {
+                                map[i][g] = '.';
+                                map[i][g + 1] = '@';
+                            },
+                            '[' => {
+                                for search_g in (g + 3)..(map[i].len() - 1) {
+                                    println!("Checking ({i}, {search_g}) ({})", map[i][search_g]);
+
+                                    if map[i][search_g] == '#' {
+                                        break;
+                                    }
+                                    if map[i][search_g] == '.' {
+                                        for move_g in ((g + 1)..=search_g).rev() {
+                                            map[i][move_g ] = map[i][move_g - 1];
+                                        }
+                                        map[i][g + 1] = '@';
+                                        map[i][g] = '.';
+                                        break;
+                                    }
+                                }
+                            },
+                            _ => {}
+                        }
+
+                        break 'outer;
+                    }
+                }
+            },
+            '^' => {
+                'outer: for i in 0..map.len() {
+                    for g in 0..map[i].len() {
+                        if map[i][g] != '@' { continue; }
+
+                        if i == 0 {
+                            continue 'move_loop;
+                        }
+
+                        let up = map[i - 1][g];
+                        println!("Character up: {up}");
+                        match up {
+                            '.' => {
+                                map[i][g] = '.';
+                                map[i - 1][g] = '@';
+                            },
+                            ']' | '[' => {
+                                let mut ind = 0;
+                                let mut can_move = true;
+                                let mut nodes: Vec<(usize, usize)> = Vec::new();
+                                nodes.push((i - 1, g));
+                                if up == ']' {
+                                    nodes.push((i - 1, g - 1));
+                                } else {
+                                    nodes.push((i - 1, g + 1));
+                                }
+
+                                while can_move && ind < nodes.len() {
+                                    let (node_i, node_g) = nodes[ind];
+
+                                    println!("Checking above ({}, {}) ({})", node_g, node_i, map[node_i - 1][node_g]);
+
+                                    if node_i == 0 {
+                                        can_move = false;
+                                        break;
+                                    }
+                                    
+                                    match map[node_i - 1][node_g]  {
+                                        '#' => {
+                                            can_move = false;
+                                            break;
+                                        },
+                                        '[' => {
+                                            if !nodes.contains(&(node_i - 1, node_g)) {
+                                                nodes.push((node_i - 1, node_g));
+
+                                            }
+                                            if !nodes.contains(&(node_i - 1, node_g + 1)) {
+                                                nodes.push((node_i - 1, node_g + 1));
+                                            }
+                                        },
+                                        ']' => {
+                                            if !nodes.contains(&(node_i - 1, node_g)) {
+                                                nodes.push((node_i - 1, node_g));
+
+                                            }
+                                            if !nodes.contains(&(node_i - 1, node_g - 1)) {
+                                                nodes.push((node_i - 1, node_g - 1));
+                                            }
+                                        },
+                                        _ => {}
+                                    }
+
+                                    ind += 1;
+                                }
+
+                                println!("Could move? {can_move} {:?}", nodes);
+
+                                if can_move {
+                                    for ind in (0..nodes.len()).rev() {
+                                        let (node_i, node_g) = nodes[ind];
     
+                                        map[node_i - 1][node_g] = map[node_i][node_g];
+                                        map[node_i][node_g] = '.';
+                                        println!("{ind}");
+                                    }
 
-    robot.render(
-        &map
-    );
+                                    map[i][g] = '.';
+                                    map[i - 1][g] = '@';
+                                }
+                            }
+                            _ => {}
+                        }
 
-    for &mv in &moves {
-        robot.mv( 
-            &mut map,
-            mv
-        );
+                        break 'outer;
+                    }
+                }
+            },
+            'v' => {
+                'outer: for i in 0..map.len() {
+                    for g in 0..map[i].len() {
+                        if map[i][g] != '@' { continue; }
 
-        //robot.render(
-        //    &map
-        //);
+                        if i == map.len() - 1 {
+                            continue 'move_loop;
+                        }
+
+                        let down = map[i + 1][g];
+                        //println!("Character down: {down}");
+                        match down {
+                            '.' => {
+                                map[i][g] = '.';
+                                map[i + 1][g] = '@';
+                            },
+                            ']' | '[' => {
+                                let mut ind = 0;
+                                let mut can_move = true;
+                                let mut nodes: Vec<(usize, usize)> = Vec::new();
+                                nodes.push((i + 1, g));
+                                if down == ']' {
+                                    nodes.push((i + 1, g - 1));
+                                } else {
+                                    nodes.push((i + 1, g + 1));
+                                }
+
+                                while can_move && ind < nodes.len() {
+                                    let (node_i, node_g) = nodes[ind];
+
+                                    println!("Checking below ({}, {}) ({})", node_g, node_i, map[node_i + 1][node_g]);
+
+                                    if node_i == map.len() - 1 {
+                                        can_move = false;
+                                        break;
+                                    }
+                                    
+                                    match map[node_i + 1][node_g]  {
+                                        '#' => {
+                                            can_move = false;
+                                            break;
+                                        },
+                                        '[' => {
+                                            if !nodes.contains(&(node_i + 1, node_g)) {
+                                                nodes.push((node_i + 1, node_g));
+
+                                            }
+                                            if !nodes.contains(&(node_i + 1, node_g + 1)) {
+                                                nodes.push((node_i + 1, node_g + 1));
+                                            }
+                                        },
+                                        ']' => {
+                                            if !nodes.contains(&(node_i + 1, node_g)) {
+                                                nodes.push((node_i + 1, node_g));
+
+                                            }
+                                            if !nodes.contains(&(node_i + 1, node_g - 1)) {
+                                                nodes.push((node_i + 1, node_g - 1));
+                                            }
+                                        },
+                                        _ => {}
+                                    }
+
+                                    ind += 1;
+                                }
+
+                                //println!("Could move? {can_move} {:?}", nodes);
+
+                                if can_move {
+                                    for ind in (0..nodes.len()).rev() {
+                                        let (node_i, node_g) = nodes[ind];
+    
+                                        map[node_i + 1][node_g] = map[node_i][node_g];
+                                        map[node_i][node_g] = '.';
+                                        //println!("{ind}");
+                                    }
+
+                                    map[i][g] = '.';
+                                    map[i + 1][g] = '@';
+                                }
+                            }
+                            _ => {}
+                        }
+
+                        break 'outer;
+                    }
+                }
+            }
+            _ => { println!("^^'"); }
+        }
+
+        //print(&map);
+
     }
-
-    let mut sum = 0usize;
+    let mut gps_coordinate_sum = 0usize;
     for (i, row) in map.iter().enumerate() {
         for (g, node) in row.iter().enumerate() {
-            if let Node::Box = node {
-                sum += 100 * i + g;
+            if *node != '[' {
+                continue;
             }
+
+            gps_coordinate_sum += 100 * i + g;
         }
     }
-    println!("GPS coordinate sum: {sum}");
+
+    println!("Final GPS coordinate sum: {gps_coordinate_sum}");
 }
