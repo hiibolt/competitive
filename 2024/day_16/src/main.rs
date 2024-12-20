a14fe5c545ef45215ca32519780646e459c0d46f
hiibolt <me@hiibolt.com>
2024-12-19 19:33:09 -0600

    feat: Completed part 2 of `day_16` for 2024

--- 2024/day_16/src/main.rs
+++ 2024/day_16/src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::HashSet;
+use std::io::Write;
 const MOVEABLES: [char; 2] = ['E', '.'];
 
 struct Node {
     val: char,
+    seat: bool,
     north_record: Option<usize>,
     east_record:  Option<usize>,
     south_record: Option<usize>,
@@ -13,6 +16,85 @@ enum Facing {
     East,
     South,
     West
+}
+fn navigate_for_score (
+    target_score: usize,
+    map: &mut Vec<Vec<Node>>,
+    score: usize,
+    i: usize,
+    g: usize,
+    facing: Facing,
+    mut visited: HashSet<(usize, usize)>
+) {
+    //println!("Navigated to ({g}, {i}) ({})", map[i][g].val);
+
+    if map[i][g].val == 'E' {
+        println!("Hit `E` with a score of {score}");
+
+        if score == target_score {
+            for (node_i, node_g) in visited.into_iter() {
+                map[node_i][node_g].seat = true;
+            }
+        }
+        
+        return;
+    }
+    
+    if score >= target_score {
+        //println!("Not the path, aborting");
+        return;
+    }
+
+    visited.insert((i, g));
+    let up_score = score + 1 + cost_to_rotate_to(facing.clone(), Facing::North);
+    if ( map[i - 1][g].north_record.is_some() && up_score == map[i - 1][g].north_record.unwrap() ) && up_score <= target_score && i != 0 && MOVEABLES.contains(&(map[i - 1][g].val)) && !visited.contains(&(i - 1, g)) {
+        navigate_for_score (
+            target_score,
+            map,
+            up_score,
+            i - 1,
+            g,
+            Facing::North,
+            visited.clone()
+        );
+    }
+    let right_score = score + 1 + cost_to_rotate_to(facing.clone(), Facing::East);
+    if map[i][g + 1].east_record.is_some() && right_score <= target_score && g != map[i].len() - 1 && MOVEABLES.contains(&(map[i][g + 1].val)) && !visited.contains(&(i, g + 1)) {
+        navigate_for_score (
+            target_score,
+            map,
+            right_score,
+            i,
+            g + 1,
+            Facing::East,
+            visited.clone()
+        );
+    }
+    let left_score = score + 1 + cost_to_rotate_to(facing.clone(), Facing::West);
+    if map[i][g - 1].west_record.is_some() && left_score <= target_score && g != 0 && MOVEABLES.contains(&(map[i][g - 1].val)) && !visited.contains(&(i, g - 1)) {
+        navigate_for_score (
+            target_score,
+            map,
+            left_score,
+            i,
+            g - 1,
+            Facing::West,
+            visited.clone()
+        );
+    }
+    let down_score = score + 1 + cost_to_rotate_to(facing.clone(), Facing::South);
+    if map[i + 1][g].south_record.is_some() && down_score <= target_score && i != map.len() - 1 && MOVEABLES.contains(&(map[i + 1][g].val)) && !visited.contains(&(i + 1, g))  {
+        navigate_for_score (
+            target_score,
+            map,
+            down_score,
+            i + 1,
+            g,
+            Facing::South,
+            visited
+        );
+    }
+
 }
 fn navigate (
     lowest_score: &mut Option<usize>,
@@ -58,7 +140,8 @@ fn navigate (
     }
     let expected_right_score = score + 1 + cost_to_rotate_to(facing.clone(), Facing::East);
     if g != map[i].len() - 1 && MOVEABLES.contains(&(map[i][g + 1].val)) && 
-    ( map[i][g + 1].east_record.is_none() || expected_up_score < map[i][g + 1].east_record.unwrap() ) {
+        ( map[i][g + 1].east_record.is_none() || expected_right_score < map[i][g + 1].east_record.unwrap() )
+    {
         map[i][g + 1].east_record = Some(expected_right_score);
         navigate (
             lowest_score,
@@ -71,7 +154,7 @@ fn navigate (
     }
     let expected_left_score = score + 1 + cost_to_rotate_to(facing.clone(), Facing::West);
     if g != 0 && MOVEABLES.contains(&(map[i][g - 1].val)) && 
-        ( map[i][g - 1].west_record.is_none() || expected_up_score < map[i][g - 1].west_record.unwrap() ) 
+        ( map[i][g - 1].west_record.is_none() || expected_left_score < map[i][g - 1].west_record.unwrap() ) 
     {
         map[i][g - 1].west_record = Some(score);
         navigate (
@@ -85,7 +168,7 @@ fn navigate (
     }
     let expected_down_score = score + 1 + cost_to_rotate_to(facing.clone(), Facing::South);
     if i != map.len() - 1 && MOVEABLES.contains(&(map[i + 1][g].val)) && 
-        ( map[i + 1][g].south_record.is_none() || expected_up_score < map[i + 1][g].south_record.unwrap() ) 
+        ( map[i + 1][g].south_record.is_none() || expected_down_score < map[i + 1][g].south_record.unwrap() ) 
     {
         map[i + 1][g].south_record = Some(expected_down_score);
         navigate (
@@ -125,6 +208,7 @@ fn main() {
             line.chars()
                 .map(|ch| Node {
                     val: ch,
+                    seat: false,
                     north_record: None,
                     east_record:  None,
                     south_record: None,
@@ -159,5 +243,52 @@ fn main() {
         Facing::East
     );
 
-    println!("Lowest score: {lowest_score:?}");
+    if let Some(lowest_score) = lowest_score {
+        println!("Lowest score: {lowest_score:?}");
+
+        navigate_for_score(
+            lowest_score, 
+            &mut map,
+            0,
+            start_i,
+            start_g,
+            Facing::East,
+            HashSet::new()
+        );
+    } else {
+        println!("No valid path!");
+
+        return;
+    }
+
+    let mut tiles = 1usize;
+    for i in 0..map.len() {
+        for g in 0..map[i].len() {
+            if map[i][g].seat {
+                print!("O");
+                tiles += 1;
+
+                continue;
+            }
+            print!("{}", map[i][g].val);
+        }
+        println!("");
+    }
+
+    println!("There are {tiles} tiles ^^");
+
+    let mut file = std::fs::File::create("output.txt").unwrap();
+
+    for i in 0..map.len() {
+        for g in 0..map[i].len() {
+            write!(file, " {}|{:05}-{:05}-{:05}-{:05}", 
+                map[i][g].val, 
+                map[i][g].north_record.unwrap_or(0),
+                map[i][g].east_record.unwrap_or(0),
+                map[i][g].south_record.unwrap_or(0),
+                map[i][g].west_record.unwrap_or(0),
+            ).unwrap();
+        }
+        write!(file, "\n").unwrap();
+    }
 }
