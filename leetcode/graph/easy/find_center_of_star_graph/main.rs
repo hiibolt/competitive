93b7ba67e6e78a3449b34357579f7c9d18915d9c
hiibolt <me@hiibolt.com>
2025-06-02 14:01:42 -0500

    feat: LC 1971

---
+++ leetcode/graph/easy/find_center_of_star_graph/main.rs
@@ -0,0 +1,29 @@
+/**
+ * 1971. "Find Center of Star Graph"
+ * 
+ * Difficulty: Easy
+ * Tags: Graph
+ * Runtime: Beats 100%
+ */
+
+use std::collections::HashSet;
+impl Solution {
+    pub fn find_center(
+        edges: Vec<Vec<i32>>
+    ) -> i32 {
+        let mut connections = HashSet::new();
+
+        for edge in edges {
+            if connections.contains(&edge[0]) {
+                return edge[0];
+            }
+            if connections.contains(&edge[1]) {
+                return edge[1];
+            }
+            connections.insert(edge[0]);
+            connections.insert(edge[1]);
+        }
+        
+        -1i32
+    }
+}
